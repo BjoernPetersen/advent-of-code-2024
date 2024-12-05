b3f868df8567bc4fe08ab752231d779430c2467e
Björn Petersen <git@bjoernpetersen.net>
2024-12-05 07:51:09 +0100

    Speed up S3 sync

--- tool/sync.dart
+++ tool/sync.dart
@@ -40,11 +40,13 @@ Future<void> _upload(RemoteClient client, Directory dir) async {
     exit(1);
   }
 
+  final tasks = <Future<void>>[];
   for (final entity in dir.listSync()) {
     if (entity is File) {
       final basename = entity.path.substring(entity.path.lastIndexOf('/') + 1);
       print('Writing ${entity.path} to cloud');
-      await client.writeFile(basename, entity);
+      tasks.add(client.writeFile(basename, entity));
     }
   }
+  await Future.wait(tasks);
 }
