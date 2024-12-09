7b21ef089f31010c0e37cdf5c2e1a5caebd639c9
Björn Petersen <git@bjoernpetersen.net>
2024-12-09 08:29:49 +0100

    Solve day 9 part 2

--- lib/src/days/day_09.dart
+++ lib/src/days/day_09.dart
@@ -1,16 +1,16 @@
 import 'package:aoc_core/aoc_core.dart';
 
+int naturalNumberSum(int n) {
+  return (n * (n + 1)) ~/ 2;
+}
+
+int intSum(int a, int b) {
+  return naturalNumberSum(b) - naturalNumberSum(a - 1);
+}
+
 @immutable
 final class PartOne extends IntPart {
   const PartOne();
-
-  int naturalNumberSum(int n) {
-    return (n * (n + 1)) ~/ 2;
-  }
-
-  int intSum(int a, int b) {
-    return naturalNumberSum(b) - naturalNumberSum(a - 1);
-  }
 
   @override
   Future<int> calculate(Stream<String> input) async {
@@ -50,6 +50,61 @@ final class PartOne extends IntPart {
       sum += position * rightFileId;
       position += 1;
       rightCursorRemaining -= 1;
+    }
+
+    return sum;
+  }
+}
+
+@immutable
+final class PartTwo extends IntPart {
+  const PartTwo();
+
+  @override
+  Future<int> calculate(Stream<String> input) async {
+    final line = await input.single;
+
+    final movedFiles = <int>{};
+    final rightCursorStart =
+        line.length % 2 == 0 ? line.length - 2 : line.length - 1;
+
+    var sum = 0;
+    var position = 0;
+
+    for (var leftCursor = 0; leftCursor < line.length; ++leftCursor) {
+      final leftLength = int.parse(line[leftCursor]);
+      final isFree = leftCursor % 2 == 1;
+
+      if (isFree) {
+        var posOffset = 0;
+        for (var rightCursor = rightCursorStart;
+            rightCursor > leftCursor && posOffset < leftLength;
+            rightCursor -= 2) {
+          final rightFileId = rightCursor ~/ 2;
+          if (movedFiles.contains(rightFileId)) {
+            continue;
+          }
+
+          final rightLength = int.parse(line[rightCursor]);
+          if (rightLength > leftLength - posOffset) {
+            continue;
+          }
+
+          final actualPosition = position + posOffset;
+
+          movedFiles.add(rightFileId);
+          sum += rightFileId *
+              intSum(actualPosition, actualPosition + rightLength - 1);
+          posOffset += rightLength;
+        }
+      } else {
+        final fileId = leftCursor ~/ 2;
+        if (!movedFiles.contains(fileId)) {
+          sum += fileId * intSum(position, position + leftLength - 1);
+        }
+      }
+
+      position += leftLength;
     }
 
     return sum;
--- test/days/day_09_test.dart
+++ test/days/day_09_test.dart
@@ -38,7 +38,7 @@ void main() {
         });
 
         for (final (example, expectedResult) in [
-          ('instructions-1', 0),
+          ('instructions-1', 2858),
         ]) {
           test('example $example passes', () {
             final reader = getExampleReader(dayNum, example);
@@ -51,7 +51,7 @@ void main() {
 
         test('input passes', () {
           final reader = getInputReader(dayNum);
-          expect(part.calculate(reader.readLines()), completion(0));
+          expect(part.calculate(reader.readLines()), completion(6415666220005));
         });
       },
       skip: day.partTwo == null,
