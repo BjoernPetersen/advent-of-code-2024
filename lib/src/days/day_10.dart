b86e841894366cf0946e6b36953b79f8d2e5f296
Björn Petersen <git@bjoernpetersen.net>
2024-12-10 06:44:32 +0100

    Create Grid.fromStream

--- lib/src/days/day_10.dart
+++ lib/src/days/day_10.dart
@@ -1,26 +1,21 @@
 import 'package:aoc_core/aoc_core.dart';
 
 Future<(Grid<int>, List<Vector>)> _buildGrid(Stream<String> input) async {
-  final rows = <List<int>>[];
   final trailheads = <Vector>[];
-
-  await for (final line in input) {
-    final row = <int>[];
-    for (final char in line.chars) {
-      final height = int.tryParse(char);
-      if (height == null) {
-        row.add(-1);
-        continue;
-      }
-
-      if (height == 0) {
-        trailheads.add(Vector(x: row.length, y: rows.length));
-      }
-      row.add(height);
+  final grid = await Grid.fromStream(input, (position, char) {
+    final height = int.tryParse(char);
+    if (height == null) {
+      return -1;
     }
-    rows.add(row);
-  }
-  return (Grid(rows), trailheads);
+
+    if (height == 0) {
+      trailheads.add(position);
+    }
+
+    return height;
+  });
+
+  return (grid, trailheads);
 }
 
 int _scoreTrailhead(
