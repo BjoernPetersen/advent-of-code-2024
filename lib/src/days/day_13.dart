3186a15c9086af83240aedad9bddb3465d363c03
Björn Petersen <git@bjoernpetersen.net>
2024-12-13 10:53:51 +0100

    Solve day 13 part 1

---
+++ lib/src/days/day_13.dart
@@ -0,0 +1,107 @@
+import 'package:aoc_core/aoc_core.dart';
+
+@immutable
+final class Machine {
+  final Vector aEffect;
+  final Vector bEffect;
+  final Vector prize;
+
+  const Machine({
+    required this.aEffect,
+    required this.bEffect,
+    required this.prize,
+  });
+
+  @override
+  String toString() {
+    return 'A: $aEffect\nB: $bEffect\nPrize: $prize';
+  }
+}
+
+final effectRegex = RegExp(r': X\+(?<x>\d+), Y\+(?<y>\d+)');
+final prizeRegex = RegExp(r': X=(?<x>\d+), Y=(?<y>\d+)');
+
+Vector _parseVector(String line, RegExp regex) {
+  final match = regex.firstMatch(line);
+  if (match == null) {
+    throw ArgumentError.value(line, 'line', 'Does not have expected format');
+  }
+
+  return Vector(
+    x: int.parse(match.namedGroup('x')!),
+    y: int.parse(match.namedGroup('y')!),
+  );
+}
+
+Stream<Machine> _parseMachines(Stream<String> lines) async* {
+  var aEffect = Vector.zero;
+  var bEffect = Vector.zero;
+
+  var counter = 0;
+  await for (final line in lines) {
+    switch (counter++) {
+      case 0:
+        aEffect = _parseVector(line, effectRegex);
+      case 1:
+        bEffect = _parseVector(line, effectRegex);
+      case 2:
+        yield Machine(
+          aEffect: aEffect,
+          bEffect: bEffect,
+          prize: _parseVector(line, prizeRegex),
+        );
+      case 3:
+        // Empty line
+        counter = 0;
+    }
+  }
+}
+
+@immutable
+final class PartOne extends IntPart {
+  const PartOne();
+
+  int _calculateB(Machine machine) {
+    final a = machine.aEffect;
+    final b = machine.bEffect;
+    final p = machine.prize;
+    final dividend = (a.x * p.y - p.x * a.y);
+    final divisor = (a.x * b.y - b.x * a.y);
+
+    if (dividend % divisor != 0) {
+      return -1;
+    }
+
+    return dividend ~/ divisor;
+  }
+
+  int _calculateA(Machine machine, int b) {
+    final dividend = machine.prize.x - (b * machine.bEffect.x);
+    if (dividend % machine.aEffect.x != 0) {
+      return -1;
+    }
+
+    return dividend ~/ machine.aEffect.x;
+  }
+
+  @override
+  Future<int> calculate(Stream<String> input) async {
+    var costSum = 0;
+    await for (final machine in _parseMachines(input)) {
+      final b = _calculateB(machine);
+      if (b < 0) {
+        // Not possible
+        continue;
+      }
+
+      final a = _calculateA(machine, b);
+      if (a < 0) {
+        // Not possible
+        continue;
+      }
+
+      costSum += a * 3 + b;
+    }
+    return costSum;
+  }
+}
---
+++ test/days/day_13_test.dart
@@ -0,0 +1,62 @@
+import 'package:aoc/day.dart';
+import 'package:test/test.dart';
+
+import '../input_helper.dart';
+
+void main() {
+  final dayNum = 13;
+  final day = getDay(dayNum);
+
+  group('day $dayNum', () {
+    group('part 1', () {
+      final part = day.partOne as IntPart;
+
+      for (final (example, expectedResult) in [
+        ('instructions-1', 280),
+        ('instructions-2', 200),
+        ('instructions-3', 480),
+      ]) {
+        test('example $example passes', () {
+          final reader = getExampleReader(dayNum, example);
+          expect(
+            part.calculate(reader.readLines()),
+            completion(expectedResult),
+          );
+        });
+      }
+
+      test('input passes', () {
+        final reader = getInputReader(dayNum);
+        expect(part.calculate(reader.readLines()), completion(36571));
+      });
+    });
+    group(
+      'part 2',
+      () {
+        late final IntPart part;
+
+        setUpAll(() {
+          part = day.partTwo as IntPart;
+        });
+
+        for (final (example, expectedResult) in [
+          ('instructions-1', 0),
+        ]) {
+          test('example $example passes', () {
+            final reader = getExampleReader(dayNum, example);
+            expect(
+              part.calculate(reader.readLines()),
+              completion(expectedResult),
+            );
+          });
+        }
+
+        test('input passes', () {
+          final reader = getInputReader(dayNum);
+          expect(part.calculate(reader.readLines()), completion(0));
+        });
+      },
+      skip: day.partTwo == null,
+    );
+  });
+}
