fb2d804e369d95b36edd39f41a17ac924e430837
Björn Petersen <git@bjoernpetersen.net>
2024-12-20 16:19:08 +0100

    Solve day 19 part 2

--- test/days/day_19_test.dart
+++ test/days/day_19_test.dart
@@ -38,7 +38,7 @@ void main() {
         });
 
         for (final (example, expectedResult) in [
-          ('instructions-1', 0),
+          ('instructions-1', 16),
         ]) {
           test('example $example passes', () {
             final reader = getExampleReader(dayNum, example);
@@ -51,7 +51,8 @@ void main() {
 
         test('input passes', () {
           final reader = getInputReader(dayNum);
-          expect(part.calculate(reader.readLines()), completion(0));
+          expect(
+              part.calculate(reader.readLines()), completion(601201576113503));
         });
       },
       skip: day.partTwo == null,
