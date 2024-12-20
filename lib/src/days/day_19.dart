e35af621be409a1776d263f9b08bbbb951b06e6c
Björn Petersen <git@bjoernpetersen.net>
2024-12-20 16:23:52 +0100

    Optimize day 19 part 1

--- lib/src/days/day_19.dart
+++ lib/src/days/day_19.dart
@@ -27,46 +27,18 @@ Future<({List<String> towels, List<String> designs})> _parseInput(
 final class PartOne extends IntPart {
   const PartOne();
 
-  RegExp _compilePattern(Map<int, List<String>> buckets, int maxIndex) {
-    final patterns = <String>[];
-    for (var index = 0; index < maxIndex; index += 1) {
-      final bucket = buckets[index];
-      if (bucket != null) {
-        patterns.addAll(bucket);
-      }
-    }
-    return RegExp('^(?:${patterns.join("|")})+\$');
-  }
-
   @override
   Future<int> calculate(Stream<String> input) async {
     final (towels: towels, designs: designs) = await _parseInput(input);
-
-    final towelBuckets = towels.groupListsBy((t) => t.length);
-    for (final bucketIndex
-        in towelBuckets.keys.sorted((a, b) => b.compareTo(a))) {
-      final bucket = towelBuckets[bucketIndex]!;
-      final smallerPattern = _compilePattern(towelBuckets, bucketIndex);
-      for (var patternIndex = bucket.length - 1;
-          patternIndex >= 0;
-          patternIndex -= 1) {
-        if (smallerPattern.hasMatch(bucket[patternIndex])) {
-          bucket.removeAt(patternIndex);
-        }
-      }
-    }
-
-    final dedupedTowels = towelBuckets.values.flattenedToList;
-
-    final pattern = RegExp('^(?:${dedupedTowels.join("|")})+?\$');
-    print(pattern.pattern);
-    return designs.where(pattern.hasMatch).count;
+    return designs
+        .where((design) => calcNumberOfCombos(design, towels, {}) > 0)
+        .count;
   }
 }
 
 int calcNumberOfCombos(
   String design,
-  Set<String> towels,
+  Iterable<String> towels,
   Map<String, int> cache,
 ) {
   if (design.isEmpty) {
@@ -101,8 +73,6 @@ final class PartTwo extends IntPart {
   @override
   Future<int> calculate(Stream<String> input) async {
     final (towels: towels, designs: designs) = await _parseInput(input);
-    return designs
-        .map((design) => calcNumberOfCombos(design, towels.toSet(), {}))
-        .sum;
+    return designs.map((design) => calcNumberOfCombos(design, towels, {})).sum;
   }
 }
